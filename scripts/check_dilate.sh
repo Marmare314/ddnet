61f56a2f4adcb7b2174fb21ac4c1467f3b6714ce
marmare314 <49279081+Marmare314@users.noreply.github.com>
2023-10-08 08:59:23 +0200

    update style.yml

--- scripts/check_dilate.sh
+++ scripts/check_dilate.sh
@@ -2,13 +2,11 @@
 #set -x
 result=
 
-dil_path=$1
-
-result=$(find "$2" -iname '*.png' -print0 | while IFS= read -r -d $'\0' file; do
+result=$(find "$1" -iname '*.png' -print0 | while IFS= read -r -d $'\0' file; do
 	new_file=$(mktemp --tmpdir "$(basename "$file" .png).XXX.png")
 	cp "$file" "$new_file"
 	convert "$new_file" "${new_file}_old.bmp" > /dev/null
-	"${dil_path}"/dilate "$new_file" > /dev/null
+	dilate "$new_file" > /dev/null
 	convert "$new_file" "${new_file}_new.bmp" > /dev/null
 	orig_hash=$(identify -quiet -format "%#" "${new_file}_old.bmp")
 	new_hash=$(identify -quiet -format "%#" "${new_file}_new.bmp")
